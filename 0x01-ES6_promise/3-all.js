c441af801c4c1369ff6bed63f9d7dadcd5de6ded
Maddie <115709272+Maddily@users.noreply.github.com>
2024-05-28 19:58:35 +0300

    Use template literal

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((result) => {
-      console.log(
-        result[0].body,
-        result[1].firstName,
-        result[1].lastName,
-      );
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch(() => { // eslint-disable-line no-unused-vars
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
