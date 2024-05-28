29a0a2f426d5add2f343a8cf2292ef358761e133
Maddie <115709272+Maddily@users.noreply.github.com>
2024-05-28 19:47:14 +0300

    Move finally statement to the end

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.finally(() => console.log('Got a response from the API'))
-    .then(() => ({ // eslint-disable-line no-unused-vars
+  return promise
+    .then(() => ({
       status: 200,
       body: 'Success',
     }))
-    .catch(() => new Error()); // eslint-disable-line no-unused-vars
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
