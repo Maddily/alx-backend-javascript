0213d3ce25c5b635fd7e08b56a9242df6f480489
Maddie <115709272+Maddily@users.noreply.github.com>
2024-05-30 17:10:21 +0300

    Change the default string description of a class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+import { inspect } from 'util';
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  [inspect.custom]() {
+    return `${this.constructor.name} [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
