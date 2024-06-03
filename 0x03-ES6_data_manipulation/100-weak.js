81702d2584ef01af730e7a2cd427cdf72ab98575
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-03 21:43:38 +0300

    Undo endpoint count initialization

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,8 +3,6 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const count = weakMap.get(endpoint) || 0;
 
-  if (count === 0) weakMap.set(endpoint, count);
-
   if (count + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
