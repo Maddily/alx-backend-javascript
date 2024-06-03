3923745f6098d7ccd0a2789dc918a8471dd29ed7
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-03 19:35:35 +0300

    Throw an error for out of bound offsets

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
   const { buffer } = new Int8Array(length);
 
   const dataView = new DataView(buffer);
   dataView.setInt8(position, value);
-
-  if (dataView.getInt8(position) !== value) console.log('Position outside range');
 
   return dataView;
 }
