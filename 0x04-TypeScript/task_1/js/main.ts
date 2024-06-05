0016fa350a96757b616f2ef8d6786b8eb85b9d0c
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-05 18:24:03 +0300

    Write a function returning a teacher's name

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,16 @@ interface Teacher {
   [propName: string]: any;
 }
 
+/**
+ * Represents a teacher object.
+ * @property {string} firstName - The first name of the teacher.
+ * @property {string} lastName - The last name of the teacher.
+ * @property {boolean} fullTimeEmployee - Indicates if the teacher is a full-time employee.
+ * @property {string} location - The location of the teacher.
+ * @property {number} [yearsOfExperience] - The number of years of experience of the teacher (optional).
+ * @property {any} [propName] - Additional properties of the teacher.
+ */
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -37,3 +47,19 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+/**
+ * Represents a function that prints the teacher's name.
+ * @param {string} firstName - The first name of the teacher.
+ * @param {string} lastName - The last name of the teacher.
+ * @returns {string} The formatted name of the teacher.
+ */
+const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher("John", "Doe"));
