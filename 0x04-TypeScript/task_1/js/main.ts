7beb7ad2d51ef1e2a546e254e60c035f5acd27e4
Maddie <115709272+Maddily@users.noreply.github.com>
2024-06-05 18:14:04 +0300

    Create a Directors interface extending Teacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,13 @@
+/**
+ * Represents a teacher.
+ */
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   location: string;
   yearsOfExperience?: number;
+  // Additional properties of the teacher.
   [propName: string]: any;
 }
 
@@ -16,3 +20,20 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+/**
+ * Represents a director who is also a teacher.
+ */
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
