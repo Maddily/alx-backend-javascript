fa45c3bfe065c394b81981e6d1ad1cf0765af91a
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-19 14:41:18 +0300

    Add an async function to parse data from a file

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,29 @@
+const fs = require('node:fs/promises');
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, { encoding: 'utf8' });
+
+    const students = data.trim().split('\n').map((line) => line.split(','));
+    students.shift();
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = students.reduce((acc, student) => {
+      const field = student[3];
+      if (!acc[field]) {
+        acc[field] = { count: 0, names: [] };
+      }
+      acc[field].count += 1;
+      acc[field].names.push(student[0]);
+      return acc;
+    }, {});
+
+    Object.entries(fields).forEach(([field, info]) => {
+      console.log(`Number of students in ${field}: ${info.count}. List: ${info.names.join(', ')}`);
+    });
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
