c2b785e409e69f1810339b540decebc3840661b8
Maddie <115709272+Maddily@users.noreply.github.com>
2024-08-20 10:06:41 +0300

    Fix typo in header

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -35,7 +35,7 @@ const port = 1245;
 
 const app = createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'test/plain');
+  res.setHeader('Content-Type', 'text/plain');
 
   if (req.url === '/') {
     res.end('Hello Holberton School!');
